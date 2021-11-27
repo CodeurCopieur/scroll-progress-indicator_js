022e234c88bb4077576cb30f45670966a01df36b
codwerk <louisws@outlook.fr>
2021-11-27 22:13:06 +0100

    Scroll Progress Indicator end

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const bar = document.querySelector('.fill');
+const h1 = document.querySelector('h1');
+
+
+
+addEventListener('scroll', ()=> {
+
+  const tl = gsap.timeline({
+    default:{
+      ease: 'power2'
+    }
+  })
+
+
+  const { scrollTop, scrollHeight } = document.documentElement;
+  const pourcentage = (scrollTop / (scrollHeight - window.innerHeight) ) * 100;
+
+
+  let bgColor = pourcentage > 99 ? "#c00" : "#000";
+  let txtColor = pourcentage > 99 ? "#c00" : "#fff";
+
+
+  gsap.set(h1, {
+    innerText: `${Math.round(pourcentage)}%`
+  })
+
+  tl
+  .to(h1, {color:txtColor})
+  .to(bar, {height:`${pourcentage}%`, backgroundColor: bgColor})
+})
