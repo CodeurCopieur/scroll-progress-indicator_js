b4ec987aaee655483e4b3c874f365e357782ad8c
codwerk <louisws@outlook.fr>
2021-11-28 14:01:30 +0100

    Mouse Follow

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 const bar = document.querySelector('.fill');
 const h1 = document.querySelector('h1');
+gsap.set('.percent', {
+    xPercent: -5,
+    yPercent: -5
+  });
 
 
+window.addEventListener('mousemove', (e)=> {
+  gsap.to('.percent', {x: e.clientX, y: e.clientY})
+})
 
-addEventListener('scroll', ()=> {
+addEventListener('scroll', (e)=> {
 
   const tl = gsap.timeline({
     default:{
